12999e76a828f1b6a23fd9be03bc67036f5ed6fe
yokselzok <yokselzok@gmail.com>
2016-06-20 00:02:35 +0300

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,6 @@
 
   if ( !config ) {
     console.log( 'Конфиг не найден' );
-    return;
   }
 
   var volumesContent = $.get('.volumes__list');
@@ -16,101 +15,6 @@
   var boxes = config.boxes;
 
   var volumesSum = 0;
-
-  var Calculator = {
-    signs: ['*','/','+', '-'],
-    firstSigns: ['*','/'],
-    secondSigns: ['+','-'],
-    parts: [],
-    result: 0
-  };
-
-  //------------------------------
-
-  Calculator.compute = function ( str ) {
-
-    this.str = str;
-    this.strToArr();
-    this.handleParts();
-
-    return this.result;
-  }
-
-  //------------------------------
-
-  Calculator.strToArr = function () {
-    var stack = '';
-    var parts = [];
-
-    for (var i = 0; i < this.str.length; i++) {
-      var item = this.str[i];
-
-      if ( this.signs.indexOf( item ) >= 0 ) {
-        parts.push( +stack );
-        stack = '';
-
-        parts.push( item );
-      }
-      else if ( item !== ' ' ) {
-        stack += item;
-      }
-    }
-    parts.push( +stack );
-
-    this.parts = parts;
-  }
-
-  //------------------------------
-
-  Calculator.handleParts = function () {
-    var parts = this.parts;
-    var result = 0;
-
-    this.handleBySigns( this.firstSigns );
-    this.handleBySigns( this.secondSigns );
-
-    this.result = this.parts[ 0 ];
-  }
-
-  //------------------------------
-
-  Calculator.handleBySigns = function ( currentSigns ) {
-    var parts = this.parts;
-
-    for (var k = 0; k <= parts.length; k++) {
-      var item = parts[k];
-
-      if ( currentSigns.indexOf( item ) >= 0 ) {
-        var sign = item;
-        var prev = +parts[ k - 1 ];
-        var next = +parts[ k + 1 ];
-        var mathResult = doMath( sign, prev, next);
-        var removeItems = parts.splice(k - 1, 3, mathResult);
-        k--;
-      }
-    }
-  }
-
-  //------------------------------
-
-  function doMath( sign, prev, next ) {
-    var result = 0;
-
-    if ( sign === '*' ) {
-      result = prev * next;
-    }
-    else if ( sign === '/' ) {
-      result = prev / next;
-    }
-    else if ( sign === '+' ) {
-      result = prev + next;
-    }
-    else if ( sign === '-' ) {
-      result = prev - next;
-    }
-
-    return result;
-  }
 
   //------------------------------
 
@@ -166,13 +70,5 @@
 
   sumVolumes();
   countBoxes();
-
-  //------------------------------
-
-
-  // input.elem.oninput = function () {
-  //   console.log( Calculator.compute( this.value ) );
-
-  // }
 
 })(window);
